d37c0e3569573e30848d65b812b0eec51af780b7
Laby Damaro Camara <ldamaro98@gmail.com>
2021-11-21 12:37:13 +0100

    notif call

--- firebase_login_push_notification/lib/main.dart
+++ firebase_login_push_notification/lib/main.dart
@@ -1,9 +1,34 @@
+import 'package:firebase_login_push_notification/services/AuthService.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:provider/provider.dart';
 import 'screens/login/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+//my custom notification
+import 'package:firebase_login_push_notification/push_notification/custom_notifier.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  //Notification
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
+  await flNotPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      .createNotificationChannel(chanel);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true, badge: true, sound: true);
+
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider<AuthService>.value(value: AuthService())
+    ],
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -28,8 +53,77 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification notification = message.notification;
+      AndroidNotification android = message.notification.android;
+
+      if (notification != null && android != null) {
+        flNotPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+                android: AndroidNotificationDetails(chanel.id, chanel.name,
+                    channelDescription: chanel.description,
+                    color: Colors.blue,
+                    playSound: true,
+                    icon: '@mipmap/ic_laucher')));
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      RemoteNotification notification = message.notification;
+      AndroidNotification android = message.notification.android;
+
+      if (notification != null && android != null) {
+        showAboutDialog(
+            context: context,
+            applicationName: notification.title,
+            applicationIcon: Icon(Icons.notification_add),
+            children: [
+              SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [Text(notification.body)],
+                ),
+              )
+            ]);
+      }
+    });
+  }
+
+  void showNotification() {
+    setState(() {
+      _counter++;
+    });
+
+    flNotPlugin.show(
+        0,
+        'Teste de $_counter',
+        'Comment allez-vous ?',
+        NotificationDetails(
+            android: AndroidNotificationDetails(chanel.id, chanel.name,
+                channelDescription: chanel.description,
+                importance: Importance.high,
+                color: Colors.blue,
+                playSound: true,
+                icon: '@mipmap/ic_launcher')));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Login());
+    return Scaffold(
+      body: Login(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: showNotification,
+        tooltip: 'Incrémenter',
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
